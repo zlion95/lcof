7a3215d34942cf21437ab6adb18c7cf3d0f99a8c
zlion95 <zudaojun@outlook.com>
2020-06-28 23:21:14 +0800

    Update construct function of struct TreeNode

---
+++ include/tree.h
@@ -0,0 +1,36 @@
+#include <queue>
+#include <string>
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {};
+    TreeNode(int x, TreeNode *t_left, TreeNode *t_right) : val(x), left(t_left), right(t_right) {};
+};
+
+
+inline std::string treeNodeToString(TreeNode* root) {
+    if (!root) return "[]";
+
+    std::string output = "";
+    std::queue<TreeNode*> q;
+    q.push(root);
+    while(!q.empty()) {
+        TreeNode* node = q.front();
+        q.pop();
+
+        if (node == nullptr) {
+          output += "null, ";
+          continue;
+        }
+
+        output += std::to_string(node->val) + ", ";
+        q.push(node->left);
+        q.push(node->right);
+    }
+    return "[" + output.substr(0, output.length() - 2) + "]";
+}
